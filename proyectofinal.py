89528d73d6ef24d5b77f5d47c5b5f06d724c5d16
Karla Salazar <karlas0809@gmail.com>
2021-02-01 21:52:03 -0600

    Initial Commit

--- proyectofinal.py
+++ proyectofinal.py
@@ -2,7 +2,9 @@ import os
 os.system('clear')
 # Para poder limpiar la pantalla sin necesidad de hacerlo desde el git/ os - Operating System
 
-name = "Karla Salazar"
+
+
+nombre_compania = "TICOTEK"
 
 print(name)
 
